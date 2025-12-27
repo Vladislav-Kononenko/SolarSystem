915c631ad2714ae715b4baaa32a2612a3cd6bbb0
Vladislav-Kononenko <vlad.konon2005@mail.ru>
2025-12-28 00:28:12 +0300

    Написан шаблон для проекта

---
+++ src/DrawPanel.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawPanel extends JPanel {
+    public  DrawPanel() {
+
+    }
+
+    @Override
+    public void paint(Graphics gr) {
+        super.paint(gr);
+        Graphics2D g = (Graphics2D) gr;
+        g.setColor(Color.black);
+        g.drawLine(10, 10, 200, 100);
+        g.drawRect(50, 10, 30, 100);
+        g.drawOval(50, 10, 30, 100);
+    }
+}
